b23da5ea14782e1868719d6cc9d5cde14bdd8d94
Yashwanth2323 <yashwanthreddy3984@gmail.com>
2025-11-26 22:45:22 -0800

    Add files via upload

--- linear probing.c
+++ linear probing.c
@@ -6,44 +6,44 @@ int hash(int key)
 {
 	return key % SIZE;
 }
-
 int main()
 {
 	int table[SIZE];
 	int i;
-	/* Initialize hash table */
-	for (i = 0; i < SIZE; i++)
-	{
-		table[i] = EMPTY;
-	}
-	int keys[] = {72, 27, 36, 24, 63, 81, 92, 101};
-	int n = 8;
-	printf("Inserting using Linear Probing...\n");
-	for (i = 0; i < n; i++)
-	{
-		int key = keys[i];
-		int index = hash(key);
-		int probe = 0;
-		/* Apply the formula: h(k, i) = (h'(k) + i) % SIZE */
-		while (table[(index + probe) % SIZE] != EMPTY)
-		{
-			probe++;
-			if (probe == SIZE)
-			{
-				printf("Table Overflow! Cannot insert %d\n", key);
-				break;
-			}
-		}
-		if (probe < SIZE)
-		{
-			int finalIndex = (index + probe) % SIZE;
-			table[finalIndex] = key;
-		}
-	}
-	printf("\nHash Table:\n");
-	for (i = 0; i < SIZE; i++)
-	{
-		printf("%d ", table[i]);
-	}
-	return 0;
+
+for (i = 0; i < SIZE; i++)
+{
+	table[i] = EMPTY;
+}
+int keys[] = {72, 27, 36, 24, 63, 81, 92, 101};
+int n = 8;
+printf("Inserting using Linear Probing...\n");
+for (i = 0; i < n; i++)
+{
+	int key = keys[i];
+	int index = hash(key);
+	int probe = 0;
+
+while (table[(index + probe) % SIZE] != EMPTY)
+{
+	probe++;
+if (probe == SIZE)
+{
+	printf("Table Overflow! Cannot insert %d\n", key);
+	break;
+}
+}
+
+if (probe < SIZE)
+{
+	int finalIndex = (index + probe) % SIZE;
+	table[finalIndex] = key;
+}
+}
+printf("\nHash Table:\n");
+for (i = 0; i < SIZE; i++)
+{
+	printf("%d ", table[i]);
+}
+return 0;
 }
